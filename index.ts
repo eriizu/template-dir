f086673d2b9db62be83e7ba796ecd02ba55663b3
Élise Philippe <elise.ph@icloud.com>
2020-06-28 20:58:03 +0200

    finishing touches

--- index.ts
+++ index.ts
@@ -4,72 +4,42 @@ import * as fs from "fs";
 import * as path from "path";
 import * as changeCase from "change-case";
 
-function generateImportsExports(dir: string) {
-    console.log(dir + ": candidate found.");
-    const files = fs.readdirSync(dir).filter((file) => {
-        if (file === "index.ts") return false;
-        if (file.endsWith(".ts")) return true;
-        try {
-            let fpath = path.join(dir, file);
-            let stat = fs.statSync(fpath);
-            if (stat.isDirectory()) {
-                let indexStat = fs.statSync(path.join(fpath, "index.ts"));
-                return indexStat.isFile();
-            } else {
-                return false;
-            }
-        } catch {
-            return false;
-        }
-    });
-
-    console.log(files);
-
-    let builder: string[] = ["// Automatically generated index"];
-
-    if (!files.length) {
-        builder.push("export default undefined;");
-    } else {
-        for (let filename of files) {
-            let importName: string;
-
-            if (filename.endsWith(".ts")) importName = filename.substring(0, filename.length - 3);
-            else importName = filename;
-
-            builder.push(`export * from "./${importName}";`);
-        }
-    }
-
-    let indexDest = path.join(dir, "index.ts");
-    fs.writeFileSync(indexDest, builder.join("\n"));
-    console.log(dir + ": index generated.");
-}
-
+// Would be way better off if there was some kind of array for matching and replacing patterns.
 function getNewFilename(file: string, templateString: string, replaceWith: string): string {
     let filename = path.basename(file);
 
     let teststr = changeCase.constantCase(templateString);
-    if (filename.includes(teststr)) {
-        return filename.replace(teststr, changeCase.constantCase(replaceWith));
+    while (filename.includes(teststr)) {
+        filename = filename.replace(teststr, changeCase.constantCase(replaceWith));
     }
     teststr = changeCase.snakeCase(templateString);
-    if (filename.includes(teststr)) {
-        return filename.replace(teststr, changeCase.snakeCase(replaceWith));
+    while (filename.includes(teststr)) {
+        filename = filename.replace(teststr, changeCase.snakeCase(replaceWith));
     }
     teststr = changeCase.camelCase(templateString);
-    if (filename.includes(teststr)) {
-        return filename.replace(teststr, changeCase.camelCase(replaceWith));
+    while (filename.includes(teststr)) {
+        filename = filename.replace(teststr, changeCase.camelCase(replaceWith));
     }
     teststr = changeCase.pascalCase(templateString);
-    if (filename.includes(teststr)) {
-        return filename.replace(teststr, changeCase.pascalCase(replaceWith));
+    while (filename.includes(teststr)) {
+        filename = filename.replace(teststr, changeCase.pascalCase(replaceWith));
     }
-    return null;
+    return filename == path.basename(file) ? null : filename;
 }
 
 function renameFile(file: string, templateString: string, replaceWith: string) {
     let filename = getNewFilename(file, templateString, replaceWith);
     if (filename) fs.renameSync(file, path.join(path.dirname(file), filename));
+}
+
+function transformFile(file: string, templateString: string, replaceWith: string) {
+    const data = fs.readFileSync(file);
+    const updatedContents = getNewFilename(data.toString(), templateString, replaceWith);
+
+    if (updatedContents) {
+        const fd = fs.openSync(file, "r+");
+        fs.writeSync(fd, updatedContents);
+    }
 }
 
 export default function crawl(root: string, templateString: string, replaceWith: string) {
@@ -79,12 +49,25 @@ export default function crawl(root: string, templateString: string, replaceWith:
         try {
             let stat = fs.statSync(path.join(root, file));
             if (stat.isDirectory()) crawl(path.join(root, file), templateString, replaceWith);
+            if (stat.isFile()) transformFile(path.join(root, file), templateString, replaceWith);
             renameFile(path.join(root, file), templateString, replaceWith);
-        } catch {}
+        } catch (err) {
+            console.error("Error while processing: " + file);
+            console.error(err);
+        }
     }
 }
 
-// let root: string = process.argv.length >= 3 ? process.argv[2] : ".";
-// let flagName: string = process.argv.length >= 4 ? process.argv[3] : ".auto-index";
+if (process.argv.length < 5) {
+    console.log(
+        "usage: " +
+            process.argv0 +
+            "[root directory to crawl in] [template string to replace] [string with which to replace]"
+    );
+    process.exit(1);
+}
+let root = process.argv[2];
+let templateStr = process.argv[3];
+let replaceWith = process.argv[4];
 // console.log(`Starting index generation with root: ${root} and flag name: ${flagName}.`);
-crawl("./test", "templateStr", "patate_sautee");
+crawl(root, templateStr, replaceWith);
